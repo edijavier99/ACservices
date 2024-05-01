78b76976e5478df24f31c110c9a716dd689669ea
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-05-01 12:42:41 +0100

    success alert added and message text area after submitting now show again the placeholder

--- frontend/src/components/contactform.js
+++ frontend/src/components/contactform.js
@@ -7,18 +7,12 @@ export const ContactForm  = () =>{
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('')
-    const [validated, setValidated] = useState(false);
+    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+
     
-    const handleSubmit = async (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
+    const handleSubmit = (event) => {
             event.preventDefault();
-            event.stopPropagation();
-        } else {
-            event.preventDefault(); // Evita que el formulario se envíe automáticamente
-            setValidated(true);
-    
-            // Crea un objeto con los datos del formulario
+
             const formData = {
                 first_name: firstName,
                 last_name: lastName,
@@ -27,31 +21,40 @@ export const ContactForm  = () =>{
             };
     
             try {
-                // process.env.BACKEND_URL
-                console.log("ndndkjsacbjdasbcjadsbcdjasbcjdsbcjsbcjasbcjs",process.env.REACT_APP_BACKEND_URL);
-                const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/send-email`, {
+                fetch(`${process.env.REACT_APP_BACKEND_URL}/api/send-email`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(formData)
+                })
+                .then(response => {
+                    if (response.ok) {
+                        setShowSuccessAlert(true); // Mostrar la alerta de éxito
+                        setEmail("");
+                        setFirstName("");
+                        setLastName("");
+                        setMessage("");
+                        event.target.reset();
+                        console.log(message);
+
+                        setTimeout(() => {
+                            setShowSuccessAlert(false);
+                        }, 3000);
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then(data => {
+                })
+                .catch(error => {
+                    console.error('Error al enviar el correo electrónico:', error);
                 });
-    
-                if (response.ok) {
-                    console.log('Correo electrónico enviado con éxito!');
-                    setEmail("")
-                    setFirstName("")
-                    setLastName("")
-                    setMessage("")
-                } else {
-                    const responseData = await response.json(); // Obtener el cuerpo de la respuesta como JSON
-                    console.error('Error al enviar el correo electrónico:', responseData.error || response.statusText);
-                }
             } catch (error) {
                 console.error('Error al enviar el correo electrónico:', error);
             }
-        }
     };
+    
     
     return(
         <section id="contact-body" className="d-flex flex-row col-md-11 p-3 my-5">
@@ -62,40 +65,36 @@ export const ContactForm  = () =>{
                     <p className="text-muted"><i className="fas fa-user-clock me-2"></i>No more than 24 hours  since receiving your request.</p>
                 </div>
                 <div className="col-ms-12 col-md-12 col-lg-7">
-                    <form id="contact-form" className={`needs-validation ${validated ? 'was-validated' : ''}`} noValidate onSubmit={handleSubmit}>
+                <form id="contact-form" onSubmit={handleSubmit}>
                         <div className="row g-3">
                             <div className="col-sm-6">
                                 <label htmlFor="firstName" className="form-label"><strong>First Name</strong></label>
                                 <input type="text" className="form-control" id="firstName" placeholder="First name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
-                                <div className="invalid-feedback">
-                                Valid first name is required.
-                                </div>
+                                
                             </div>
 
                             <div className="col-sm-6">
                                 <label htmlFor="lastName" className="form-label"><strong>Last Name</strong></label>
                                 <input type="text" className="form-control" id="lastName" placeholder="Last name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-                                <div className="invalid-feedback">
-                                Valid last name is required.
-                                </div>
+                                
                             </div>
 
                             <div className="col-12">
                                 <label htmlFor="email" className="form-label"><strong>Email</strong></label>
                                 <input type="email" className="form-control" id="email" placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                                <div className="invalid-feedback">
-                                Please enter a valid email addres.
-                                </div>
+                                
                             </div>
                             <label htmlFor="contact-message" className="form-label"><strong>Message</strong></label>
                             <textarea id="contact-message"  className="form-control" onChange={(e)=> setMessage(e.target.value)} rows={3} placeholder="Write here your message" required></textarea>
-                            <div className="invalid-feedback">
-                                Valid message is required.
-                                </div>
                             <hr className="mt-5" />
                             <button className="w-100 btn btn-success btn-lg" type="submit">SEND</button>
                         </div>
                     </form>
+                    {showSuccessAlert && (
+                    <div className="alert alert-success" role="alert">
+                        The email was successfully send! 
+                    </div>
+                )}
                 </div>
             </section>
     )
